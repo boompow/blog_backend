1735026ed417cf9bb10b468761e5ee59ccc59b61
boompow <gatewaymesfin@gmail.com>
2025-06-29 13:13:24 +0300

    comment handler update

--- controller/BlogWrite.js
+++ controller/BlogWrite.js
@@ -49,19 +49,53 @@ export async function commentWrite(req, res) {
       .json({ error: true, message: "Invalid Comment Format" });
   }
 
-  const { comment, author, blog } = req.body;
+  const { comment, authorID, blogID } = req.body;
   try {
     const newComment = await Comment.create({
-      comment,
-      author,
-      blog,
+      comment: comment,
+      author: authorID,
+      blog: blogID,
     });
 
-    const user = await User.findOne({ id: author });
+    // adding the comment into the blog
+    await Blog.updateOne(
+      { _id: blogID },
+      { $push: { comments: newComment._id } }
+    );
 
     res
       .status(200)
       .json({ error: false, message: "comment created successfuly" });
+  } catch (error) {
+    return res.status(500).json({ error: true, message: error });
+  }
+}
+
+export async function replyWrite(req, res) {
+  const { error } = joiCommentValidation(req.body);
+  if (error) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Invalid Comment Reply Format" });
+  }
+
+  const { commentID, authorID, blogID, reply } = req.body;
+  try {
+    const newComment = await Comment.create({
+      comment: reply,
+      author: authorID,
+      blog: blogID,
+    });
+
+    // adding the comment into the blog
+    await Comment.updateOne(
+      { _id: commentID },
+      { $push: { reply: newComment._id } }
+    );
+
+    res
+      .status(200)
+      .json({ error: false, message: "reply created successfuly" });
   } catch (error) {
     return res.status(500).json({ error: true, message: error });
   }
